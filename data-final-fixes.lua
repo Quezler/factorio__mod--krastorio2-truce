a4108c46ff255b2ae0966ef58428b14615452474
Patrick 'Quezler' Mounier <Quezler@me.com>
2020-06-22 17:49:19 +0200

    Add files via upload

---
+++ data-final-fixes.lua
@@ -0,0 +1,9 @@
+data.raw["tile"]["kr-creep"].collision_mask = { "ghost-layer", "ground-tile", "not-colliding-with-itself" }; -- "floor-layer"
+
+data.raw["technology"]["kr-creep-virus"].hidden = true;
+data.raw["capsule"   ]["kr-creep-virus"].hidden = true;
+data.raw["recipe"    ]["kr-creep-virus"].hidden = true;
+
+data.raw["technology"]["kr-biter-virus"].hidden = true;
+data.raw["capsule"   ]["kr-biter-virus"].hidden = true;
+data.raw["recipe"    ]["kr-biter-virus"].hidden = true;
